a37eba7ce543c0e6102328326398dbdc29231951
Dell <Dell@Avinash>
2025-07-03 18:04:39 +0530

    The Primitive Datatypes

---
+++ Datatypes/src/primitiveDatatypes/R.java
@@ -0,0 +1,69 @@
+package primitiveDatatypes;
+
+public class R {
+
+           // the primitive Datatypes is System defined  	
+	
+           // Eight types of primitive datatypes	
+	
+           int a=	10;         // int is shows only integer values 
+                                // has size of 4 bytes
+                                // and has range of -2,147,483,648  to 	2,147,483,647       
+               
+           byte b= 20;          // is shows integer values only but small values for memory efficiency
+                                // has the default value of 1byte or 8 bits
+                                // has the range of -128 to 127
+           
+           short c=30000;       // is shows also only integer values but max than byte and min than long
+                               // has the default value of 2bytes 
+                                // has the range of -32,768 to 32,767
+           
+           long d=655645468365465454l;       //is shows only large integer values  
+                                // has the default value of 8bytes
+                                 
+           float e=2546987741122212222222222256563222333.546114f;   
+                                //is shows the decimal values 
+                                // has the default size of 4bytes
+                                // has default value 0 and shown as 4.2546f
+            
+           double f=532.458798642312355564478946; // is also shows the decimal integers 
+                                                  // has the default size of 8bytes
+                                                    // has the default value 0
+           
+           boolean g= false;   // is shows the statement is true or false statement 
+                               // has the default size of 8bytes
+                               // has the default value "false" 
+           
+           char h='@';         // is show the single unicode
+                               // has the default size of 2 bytes 
+                               // has the default value or range is like  to 60,000
+           
+           // We can use the long datatype as for the 10 digitmobile number
+           
+           long mobNumber = 9518902242L; 
+           
+           
+           
+           
+}
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
---
+++ Datatypes/src/primitiveDatatypes/TestR.java
@@ -0,0 +1,32 @@
+package primitiveDatatypes;
+
+public class TestR {
+
+	public static void main(String[] args) {
+		
+		R Var= new R();
+		
+		System.out.println(Var.a);
+		
+		System.out.println(Var.b);
+		
+		System.out.println(Var.c);
+		
+		System.out.println(Var.d);
+		
+		System.out.println(Var.e);
+		
+		System.out.println(Var.f);
+		
+		System.out.println(Var.g);
+		
+		System.out.println(Var.h);
+
+		System.out.println("The Mobile Number Shows via Long premitive datatype");
+		
+		System.out.println(Var.mobNumber);
+		
+		
+	}
+
+}
