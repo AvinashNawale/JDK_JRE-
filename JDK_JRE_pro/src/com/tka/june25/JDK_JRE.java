a5198b8b923b918d8963fe52229c8899e4e28c2a
Dell <Dell@Avinash>
2025-07-27 09:11:09 +0530

    Java Dev Kit

---
+++ JDK_JRE_pro/src/com/tka/june25/JDK_JRE.java
@@ -0,0 +1,42 @@
+package com.tka.june25;
+
+public class JDK_JRE {
+	
+	/*
+	 * 
+	 * The JDK is a software development kit provided by Oracle (or other vendors) 
+	 * that allows us to develop, compile, and run Java programs.
+
+	📦 What JDK Includes
+	JRE (Java Runtime Environment)
+
+	Java Compiler (javac)
+
+	Java Debugger (jdb)
+
+	Other development tools (like javap, javadoc, etc.)
+
+	🧠 Use Case
+    we are a Java developer, we need the JDK to:
+
+	Write Java code
+
+	Compile Java code (.java → .class)
+
+	Run Java programs
+
+	🔁 What is JRE (Java Runtime Environment)?
+	✅ Definition
+	The JRE provides the libraries and JVM (Java Virtual Machine) to run Java applications.
+
+	📦 JRE Includes
+	JVM (Java Virtual Machine)
+
+	Core libraries 
+
+	Supporting files for runtime
+
+	
+                               */
+
+}
